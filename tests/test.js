4137dfbfe095543ef4ac708e734227cd42f8d288
Marcin Adamowski <marcin.adamowski@gmail.com>
2015-02-05 23:01:13 +0000

    add chai-test

--- tests/test.js
+++ tests/test.js
@@ -3,19 +3,19 @@ var expect = require("chai").expect;
  
 describe('Tests', function(){
   
-  describe('#indexOf()', function(){
+  describe('assert test', function(){
     it('should return -1 when the value is not present', function(){
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
     
-//  describe('#indexOf()', function(){
-//    it('should be a string', function(){
-//      var foo = 'test';
-//      expect(foo).to.be.a('string');
-//    });
-//  });
+  describe('chai test', function(){
+    it('should be a string', function(){
+      var foo = 'test';
+      expect(foo).to.be.a('string');
+    });
+  });
     
 });
 
